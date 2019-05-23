145312de94652a259d3ba2e343eb7ad36f089e4b
高橋是高 <shcrad.kore@gmail.com>
2019-05-23 18:33:27 +0900

    物件情報が削除されると同時にその物件の最寄駅情報も削除されるように。

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,4 @@
 class Property < ApplicationRecord
-  has_many :nearest_stations, inverse_of: :property
+  has_many :nearest_stations, dependent: :destroy, inverse_of: :property
   accept_nested_attributes_for :nearest_stations
 end
