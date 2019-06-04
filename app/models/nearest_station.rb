2aff62ef92705ea570fcd0a46cc7bc83861bf6fe
高橋是高 <shcrad.kore@gmail.com>
2019-06-04 17:40:45 +0900

    モデルにバリデーションを記述。

--- app/models/nearest_station.rb
+++ app/models/nearest_station.rb
@@ -1,3 +1,7 @@
 class NearestStation < ApplicationRecord
   belongs_to :property, inverse_of: :nearest_stations
+
+  validates :line, presence: true, length: {maximum: 20}
+  validates :station, presence: true, length: {maximum: 20}
+  validates :walking_minutes, presence: true, length: {maximum: 5}, numericality: true
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,10 @@
 class Property < ApplicationRecord
   has_many :nearest_stations, dependent: :destroy, inverse_of: :property
   accepts_nested_attributes_for :nearest_stations
+  
+  validates :name, presence: true, length: {maximum: 30}
+  validates :address, presence: true, length: {maximum: 50}
+  validates :price, presence: true, length: {maximum: 10}, numericality: true
+  validates :age, presence: true, length: {maximum: 3}, numericality: true
+  validates :note, presence: true, length: {maximum: 200}
 end
